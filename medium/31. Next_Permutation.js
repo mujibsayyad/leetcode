bbdf8e5f4e3d4cd4828ad6b901754b88cfe0f145
mujib sayyad <mujibsayyad97@gmail.com>
2022-08-10 22:19:25 +0530

    Next Permutation

---
+++ medium/31. Next_Permutation.js
@@ -0,0 +1,33 @@
+function swap(nums, i, j) {
+  [nums[i], nums[j]] = [nums[j], nums[i]];
+}
+
+let nextPermutation = function (nums) {
+  let pivot = -1;
+
+  for (let i = nums.length - 1; i > 0; i--) {
+    if (nums[i] > nums[i - 1]) {
+      pivot = i - 1;
+      break;
+    }
+  }
+
+  if (pivot === -1) {
+    nums.reverse();
+    return;
+  }
+
+  for (let i = nums.length - 1; i > pivot; i--) {
+    if (nums[i] > nums[pivot]) {
+      swap(nums, i, pivot);
+      break;
+    }
+  }
+
+  for (let i = pivot + 1, j = nums.length - 1; i < j; i++, j--) {
+    swap(nums, i, j);
+  }
+
+  //   T.C : O(n)
+  //   S.C : O(1)
+};
