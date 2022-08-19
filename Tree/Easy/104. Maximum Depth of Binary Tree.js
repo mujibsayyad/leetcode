0afc166b82210742812ddcbf691d584d3e16b7af
mujib sayyad <mujibsayyad97@gmail.com>
2022-08-19 23:00:33 +0530

    104. Maximum Depth of Binary Tree

---
+++ Tree/Easy/104. Maximum Depth of Binary Tree.js
@@ -0,0 +1,18 @@
+let maxDepth = function (root) {
+  let maxDepth = 0;
+
+  function traverse(root, depth) {
+    if (!root) {
+      maxDepth = Math.max(depth, maxDepth);
+      return;
+    }
+    traverse(root.left, depth + 1);
+    traverse(root.right, depth + 1);
+  }
+  traverse(root, 0);
+
+  return maxDepth;
+
+  // Time Complexity: O(n)
+  // Space Complexity: O(n), in the worst case; in case of a skewed tree
+};
