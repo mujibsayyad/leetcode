04970b8531126926e58c7177cee38ee708461a33
mujib sayyad <mujibsayyad97@gmail.com>
2022-09-07 22:46:01 +0530

    41. First Missing Positive

---
+++ Array/Hard/41. First Missing Positive.js
@@ -0,0 +1,46 @@
+let firstMissingPositive = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let idx = nums[i] - 1;
+
+    if (i === idx || nums[i] === nums[idx]) continue;
+
+    if (idx >= 0 && idx <= nums.length - 1) {
+      //swap
+      [nums[i], nums[idx]] = [nums[idx], nums[i]];
+      // decrement by 1 to recheck, is it sorted by index or not;
+      i--;
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i + 1 === nums[i]) continue;
+    else return i + 1;
+  }
+
+  return nums.length + 1;
+
+  // T.C: O(n)
+  // S.C: O(1)
+};
+
+let firstMissingPositivee = function (nums) {
+  let map = new Map();
+
+  for (let val of nums) {
+    if (val <= 0) continue;
+    else {
+      map.set(val, map.get(val) + 1 || 1);
+    }
+  }
+
+  for (let i = 1; i <= nums.length + 1; i++) {
+    if (!map.has(i)) {
+      return i;
+    }
+  }
+
+  // T.C: O(n)
+  // S.C: O(n)
+};
+
+// link :- https://leetcode.com/problems/first-missing-positive/
