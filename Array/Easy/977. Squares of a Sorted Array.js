5c1c9c10b1ea7d37074e52989e6d6bdd41605f82
mujib sayyad <mujibsayyad97@gmail.com>
2022-10-12 22:39:22 +0530

    977. Squares of a Sorted Array

--- Array/Easy/977. Squares of a Sorted Array.js
+++ Array/Easy/977. Squares of a Sorted Array.js
@@ -1,4 +1,27 @@
+// 1
 let sortedSquares = function (nums) {
+  let arr = [];
+  let left = 0,
+    right = nums.length - 1;
+
+  while (left <= right) {
+    if (nums[left] ** 2 > nums[right] ** 2) {
+      arr.push(nums[left] ** 2);
+      left++;
+    } else {
+      arr.push(nums[right] ** 2);
+      right--;
+    }
+  }
+
+  return arr.reverse();
+
+  // T.C: O(n)
+  // S.C: O(n)
+};
+
+// 2
+let sortedSquaress = function (nums) {
   let sqr = [];
 
   for (let i = 0; i < nums.length; i++) {
@@ -6,6 +29,9 @@ let sortedSquares = function (nums) {
   }
 
   return sqr.sort((a, b) => a - b);
+
+  // T.C: O(n log n)
+  // S.C: O(n)
 };
 
 // link :- https://leetcode.com/problems/squares-of-a-sorted-array/
