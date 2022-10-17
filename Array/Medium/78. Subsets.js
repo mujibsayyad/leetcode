4c9ffb42e9e92e35b724a12ab4258c78974fe852
mujib sayyad <mujibsayyad97@gmail.com>
2022-10-17 20:10:32 +0530

    78. Subsets

--- Array/Medium/78. Subsets.js
+++ Array/Medium/78. Subsets.js
@@ -1,3 +1,4 @@
+// 1 Iterative
 let subsets = function (nums) {
   let arr = [[]];
 
@@ -9,8 +10,30 @@ let subsets = function (nums) {
 
   return arr;
 
-  // T.C: O(n)
+  // T.C: O(n^2)
   // S.C: O(n)
+};
+
+// 2 Recursive
+let subsetss = function (nums) {
+  let result = [];
+
+  function recurse(i, root, arr) {
+    if (i === root.length) {
+      result.push(arr.slice());
+      return;
+    }
+    recurse(i + 1, root, arr);
+    arr.push(root[i]);
+
+    recurse(i + 1, root, arr);
+    arr.pop();
+  }
+  recurse(0, nums, []);
+  return result;
+
+  // T.C: O(2^n)
+  // S.C: O(2^n)
 };
 
 // link :- https://leetcode.com/problems/subsets/
