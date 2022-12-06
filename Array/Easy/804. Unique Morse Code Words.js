e95fde7d25abc389b60841aac2efe74f6f75a87a
mujib sayyad <mujibsayyad97@gmail.com>
2022-12-06 19:48:53 +0530

    804. Unique Morse Code Words

---
+++ Array/Easy/804. Unique Morse Code Words.js
@@ -0,0 +1,20 @@
+let uniqueMorseRepresentations = function (words) {
+    
+  let morseCode = [".-","-...","-.-.","-..",".","..-.","--.",
+  "....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",
+  ".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
+
+  let map = new Map();
+
+  for (let i = 0; i < words.length; i++) {
+    let temp = '';
+
+    for (let j = 0; j < words[i].length; j++) {
+      let char = words[i][j].charCodeAt() - 97;
+      temp += morseCode[char];
+    }
+    map.set(temp, map.get(temp) + 1 || 1);
+  }
+
+  return map.size;
+};
