058b983d1ec74abe4729285ad02eac276386e564
mujib sayyad <mujibsayyad97@gmail.com>
2022-12-17 22:25:34 +0530

    1588. Sum of All Odd Length Subarrays

---
+++ Array/Easy/1588. Sum of All Odd Length Subarrays.js
@@ -0,0 +1,14 @@
+let sumOddLengthSubarrays = function (arr) {
+  let sum = 0,
+    N = arr.length;
+  for (let i = 0; i < arr.length; i++) {
+    let total = i * (N - i) + (N - i);
+    sum += Math.ceil(total / 2) * arr[i];
+  }
+  return sum;
+};
+
+// T.C: O(N)
+// S.C: O(1)
+
+// link :- https://leetcode.com/problems/decode-xored-array/
