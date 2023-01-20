04149a299fd2eb957dec48fbde785b3ec8932d1a
mujib sayyad <mujibsayyad97@gmail.com>
2023-01-20 22:57:07 +0530

    2441. Largest Positive Integer That Exists With Its Negative

---
+++ HashMap/Easy/2441. Largest Positive Integer That Exists With Its Negative.js
@@ -0,0 +1,22 @@
+let findMaxK = function (nums) {
+  let arr = [];
+  let map = new Map();
+
+  for (let val of nums) {
+    map.set(val, map.get(val) + 1 || 1);
+  }
+
+  for (let val of nums) {
+    if ((map.has(val) && map.has(-val)) || (map.has(-val) && map.has(val))) {
+      arr.push(val);
+    }
+  }
+
+  if (arr.length < 1) return -1;
+  return Math.max(...arr);
+};
+
+// T.C: O(n)
+// S.C: O(n)
+
+// link :- https://leetcode.com/problems/largest-positive-integer-that-exists-with-its-negative/
