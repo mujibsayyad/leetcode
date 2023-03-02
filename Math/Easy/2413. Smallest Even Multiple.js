0c14cffba06c585e2125b6e8d08f361b2d47cba1
mujib sayyad <mujibsayyad97@gmail.com>
2023-03-02 22:06:27 +0530

    2413. Smallest Even Multiple

---
+++ Math/Easy/2413. Smallest Even Multiple.js
@@ -0,0 +1,9 @@
+let smallestEvenMultiple = function (n) {
+  if (n % 2 === 0) return n;
+  return n * 2;
+};
+
+// T.C: O(1)
+// S.C: O(1)
+
+// link :- https://leetcode.com/problems/smallest-even-multiple/
