537374ae63692d60fb2840cc132da64f974ed9e5
mujib sayyad <mujibsayyad97@gmail.com>
2023-03-08 22:04:34 +0530

    1323. Maximum 69 Number

---
+++ Math/Easy/1323. Maximum 69 Number.js
@@ -0,0 +1,8 @@
+let maximum69Number = function (num) {
+  return parseInt(num.toString().replace('6', '9'));
+};
+
+// T.C: O(n)
+// S.C: O(1)
+
+// link :- https://leetcode.com/problems/maximum-69-number/
