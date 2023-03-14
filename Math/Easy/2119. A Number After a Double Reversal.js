f77ccfd7157cf2216f8701e0dca326daa61199ce
mujib sayyad <mujibsayyad97@gmail.com>
2023-03-14 19:40:18 +0530

    2119. A Number After a Double Reversal

---
+++ Math/Easy/2119. A Number After a Double Reversal.js
@@ -0,0 +1,8 @@
+let isSameAfterReversals = function (num) {
+  return num === 0 || num % 10;
+};
+
+// T.C: O(1)
+// S.C: O(1)
+
+// link :- https://leetcode.com/problems/a-number-after-a-double-reversal/
