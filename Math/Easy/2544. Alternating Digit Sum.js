8603cda68b2a7f84c66f19d80b8c4e7c822722a5
mujib sayyad <mujibsayyad97@gmail.com>
2023-03-16 22:27:00 +0530

    2544. Alternating Digit Sum

---
+++ Math/Easy/2544. Alternating Digit Sum.js
@@ -0,0 +1,19 @@
+let alternateDigitSum = function (n) {
+  let total = 0;
+  let string = n.toString().split('');
+
+  for (let i = 0; i < string.length; i++) {
+    if (i % 2 === 0) {
+      total += Number(string[i]);
+    } else {
+      total -= Number(string[i]);
+    }
+  }
+
+  return total;
+};
+
+// T.C: O(n)
+// S.C: O(n)
+
+// link :- https://leetcode.com/problems/alternating-digit-sum/
